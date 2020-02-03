35fe86f4241afa0c65102eacebd7f418046a575e
Jonbeckas <Jonasbeckerx3@outlook.de>
2020-02-03 09:56:45 +0100

    change BeastTest

--- src/test/java/main/BeastTest.java
+++ src/test/java/main/BeastTest.java
@@ -1,14 +1,25 @@
 package main;
 
+import presets.Caching;
+import presets.StoreToFile;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class BeastTest {
     public static void main(String[] args) {
         Beast b = new Beast("testing");
-        BeastInstance bi = b.openInstanceAndGet(TestStorage.class,"lol");
-        b.openInstance(TestLolStorage.class,"toll");
+        BeastInstance bi = b.openInstanceAndGet(StoreToFile.class,"lol");
+        b.openInstance(Caching.class,"toll");
         BeastInstance bild = b.getBeastInstance("toll");
+        LinkedList<StoreObject> ll = new LinkedList<>();
+        ll.add(new StoreObject("hmm","oko"));
+        ll.add(new StoreObject("ok",1));
         bild.set("LOLOLOLOLO","hallo");
-        bi.set("Hallo","hi");
-        System.out.println( bi.get(String.class,"hi")+"    "+bild.get(String.class,"hallo"));
+        bi.set(ll,"hi");
+        ArrayList<Object> arrayList = bi.getList("hi");
+        System.out.println((arrayList.get(0)+" und "+arrayList.get(1)));
+        System.out.println( bi.getList("hi")+"    "+bild.get(String.class,"hallo"));
     }
 
 }
